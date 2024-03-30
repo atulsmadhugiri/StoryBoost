004e69fb5b0c09ce89e524ab46c5318023940cdf
Atul Madhugiri <atulm@berkeley.edu>
2024-03-30 11:36:22 -0400

    [ios] Initialize Xcode project

---
+++ StoryBoost/StoryBoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryBoostApp.swift
+//  StoryBoost
+//
+//  Created by atul on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct StoryBoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
