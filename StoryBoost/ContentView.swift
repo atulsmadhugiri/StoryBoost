641e3df36a87177046d14da447fb5b0970bdbff8
Atul Madhugiri <atulm@berkeley.edu>
2024-03-30 12:45:30 -0400

    [ios] Add `VideoPlayer` to `ContentView`
    Just showing placeholder video for now.

--- StoryBoost/ContentView.swift
+++ StoryBoost/ContentView.swift
@@ -1,14 +1,34 @@
+import AVKit
 import SwiftUI
 
 struct ContentView: View {
+  @State private var player = AVPlayer()
+
   var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+
+      ZStack {
+        VideoPlayer(player: player)
+          .edgesIgnoringSafeArea(.all)
+          .navigationBarBackButtonHidden()
+          .onAppear {
+            let url = URL(string: "https://blob.sh/output.mp4")
+
+            if let url {
+              player = AVPlayer(url: url)
+              player.play()
+            }
+
+          }
+          .onDisappear {
+            player.pause()
+          }.frame(width: 278, height: 600).cornerRadius(8)
+        ProgressView().progressViewStyle(CircularProgressViewStyle())
+        Color.gray.opacity(0.1).cornerRadius(8).frame(width: 360, height: 600).padding()
+      }
+
     }
-    .padding()
+
   }
 }
 
