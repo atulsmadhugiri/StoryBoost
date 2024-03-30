1b46c9e32bed280c644c0777ade9c84dc947bd10
Atul Madhugiri <atulm@berkeley.edu>
2024-03-30 13:33:54 -0400

    [ios] Get some basic image selection working

--- StoryBoost/ContentView.swift
+++ StoryBoost/ContentView.swift
@@ -1,34 +1,64 @@
 import AVKit
+import PhotosUI
 import SwiftUI
+
+struct TransferableImageSelection: Transferable {
+  let image: Data
+
+  static var transferRepresentation: some TransferRepresentation {
+    DataRepresentation(importedContentType: .image) { data in
+      if let jpegData = UIImage(data: data)?.jpegData(compressionQuality: 0.75) {
+        return TransferableImageSelection(image: jpegData)
+      } else {
+        return TransferableImageSelection(image: data)
+      }
+    }
+  }
+}
 
 struct ContentView: View {
   @State private var player = AVPlayer()
+  @State private var selectedMediaData: Data?
+  @State private var imageSelection: PhotosPickerItem?
 
   var body: some View {
     VStack {
-
-      ZStack {
-        VideoPlayer(player: player)
-          .edgesIgnoringSafeArea(.all)
-          .navigationBarBackButtonHidden()
-          .onAppear {
-            let url = URL(string: "https://blob.sh/output.mp4")
-
-            if let url {
-              player = AVPlayer(url: url)
-              player.play()
-            }
-
-          }
-          .onDisappear {
-            player.pause()
-          }.frame(width: 278, height: 600).cornerRadius(8)
-        ProgressView().progressViewStyle(CircularProgressViewStyle())
-        Color.gray.opacity(0.1).cornerRadius(8).frame(width: 360, height: 600).padding()
+      PhotosPicker(
+        selection: $imageSelection, matching: .all(of: [.images, .not(.livePhotos)]),
+        photoLibrary: .shared()
+      ) {
+        Image(systemName: "photo.circle.fill")
+          .symbolRenderingMode(.multicolor)
+          .font(.system(size: 30))
+          .foregroundColor(.blue)
       }
 
-    }
+      ZStack {
+        if let selectedMediaData {
+          if let uiImage = UIImage(data: selectedMediaData) {
+            Image(uiImage: uiImage).resizable().cornerRadius(8).aspectRatio(contentMode: .fit)
+              .frame(width: 360, height: 600).padding().scaledToFit()
+          }
+        } else {
+          Color.gray.opacity(0.1).cornerRadius(8).frame(width: 360, height: 600).padding()
+        }
+      }
 
+    }.onChange(of: imageSelection) {
+      if let imageSelection {
+        imageSelection.loadTransferable(type: TransferableImageSelection.self) { result in
+          switch result {
+          case .success(let img?):
+            self.selectedMediaData = img.image
+            print("Image selected successfully!")
+          case .success(.none):
+            print("No image selected.")
+          case .failure(_):
+            print("Image selection failed.")
+          }
+        }
+      }
+    }
   }
 }
 
