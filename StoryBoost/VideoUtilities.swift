29468ffe56a310f04fc69d3684bfac6c132f06be
Atul Madhugiri <atulm@berkeley.edu>
2024-04-06 20:54:00 -0400

    [ios] Introduce `saveVideo(url: URL)`
    Also modify `potentiallyRequestAuthorization` to only request `.addOnly` authorization which should simplify things.

--- StoryBoost/VideoUtilities.swift
+++ StoryBoost/VideoUtilities.swift
@@ -2,7 +2,28 @@ import Foundation
 import Photos
 
 func potentiallyRequestAuthorization() async -> PHAuthorizationStatus {
-  let existingStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+  let existingStatus = PHPhotoLibrary.authorizationStatus(for: .addOnly)
   if existingStatus != .notDetermined { return existingStatus }
-  return await PHPhotoLibrary.requestAuthorization(for: .readWrite)
+  return await PHPhotoLibrary.requestAuthorization(for: .addOnly)
+}
+
+enum VideoSaveError: Error {
+  case notAuthorized
+  case unknown
+}
+
+// Inspired by https://stackoverflow.com/a/76429751
+func saveVideo(url: URL) async throws {
+  let existingStatus = await potentiallyRequestAuthorization()
+  if existingStatus != .authorized {
+    throw VideoSaveError.notAuthorized
+  }
+
+  do {
+    try await PHPhotoLibrary.shared().performChanges {
+      PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: url)
+    }
+  } catch {
+    throw VideoSaveError.unknown
+  }
 }
